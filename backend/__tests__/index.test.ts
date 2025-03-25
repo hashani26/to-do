f4fa527f5ac8a34ca40f4ea05ebafa39d1601dcc
Hashani26 <hashanilakshi@gmail.com>
2025-03-25 15:53:47 +0530

    setup tests in backend

--- backend/__tests__/index.test.ts
+++ backend/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import app from "../src/index";
 
 describe("Task API", () => {
   it("should return an empty task list initially", async () => {
-    const res = await request(app).get("/tasks");
+    const res = await request(app).get("/api/tasks");
     expect(res.status).toBe(200);
     expect(res.body).toEqual([]);
   });
