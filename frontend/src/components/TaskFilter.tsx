703c5502202e31739eba375f2563ee9224bdf9fc
Hashani26 <hashanilakshi@gmail.com>
2025-03-28 18:47:35 +0530

    sorting

--- frontend/src/components/TaskFilter.tsx
+++ frontend/src/components/TaskFilter.tsx
@@ -1,11 +1,11 @@
 import { useTaskStore } from "../store/taskStore";
 
 const TaskFilters = () => {
-  const { searchQuery, priorityFilter, statusFilter, setSearchQuery, setPriorityFilter, setStatusFilter } =
+  const { searchQuery, priorityFilter, statusFilter, setSearchQuery, setPriorityFilter, setStatusFilter, setSort, sort } =
     useTaskStore();
 
   return (
-    <div className="flex flex-col sm:flex-row gap-3 mb-4">
+    <div className="flex flex-col sm:flex-row gap-3 bg-gray-100 p-4 rounded-lg mb-4">
       {/* Search Bar */}
       <input
         type="text"
@@ -28,6 +28,13 @@ const TaskFilters = () => {
         <option value="All">All Status</option>
         <option value="done">Done</option>
         <option value="not done">Pending</option>
+      </select>
+      <select value={sort} onChange={(e) => setSort(e.target.value)} className="p-2 border rounded">
+        <option value="All">Default</option>
+        <option value="pAsc">Priority Ascending</option>
+        <option value="pDsc">Priority Decending</option>
+        <option value="sAsc">Status Ascending</option>
+        <option value="sDsc">Status Decending</option>
       </select>
     </div>
   );
